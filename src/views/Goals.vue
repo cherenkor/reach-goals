0b2f24dc4326e870ac1f9fdc63631cde29438c36
Kucherenko Sergey <kucherenko.web@gmail.com>
2019-03-27 22:19:07 +0200

    Add expansion panel for goal cards

--- src/views/Goals.vue
+++ src/views/Goals.vue
@@ -3,41 +3,53 @@
     <h1 class="subheading grey--text text--darken-1">Goals</h1>
 
     <v-container>
-      <v-card
-        v-for="goal in goals"
-        :key="goal.title"
-        :style="`border-radius: 5px 0 0 5px; border-left: 10px solid ${getColorByDifficulty(goal.difficulty)}`"
-        flat
-      >
-        <v-layout row wrap class="pa-3">
-          <v-flex md3 sm3 xs12>
-            <div class="caption grey--text">Title</div>
-            <div class="mt-2">{{ goal.title }}</div>
-          </v-flex>
-          <v-flex md2 sm2 xs6>
-            <div class="caption grey--text">Difficulty</div>
-            <div class="mt-2">{{ goal.difficulty }}</div>
-          </v-flex>
-          <v-flex md2 sm2 xs6>
-            <div class="caption grey--text">Steps</div>
-            <div class="mt-2">{{ goal.steps }}</div>
-          </v-flex>
-          <v-flex md2 sm2 xs6>
-            <div class="caption grey--text">Step Duration</div>
-            <div class="mt-2">{{ goal.stepMinDuration }}m</div>
-          </v-flex>
-          <v-flex md2 sm2 xs6>
-            <div class="caption grey--text">Achived by users</div>
-            <div class="mt-2">{{ goal.added }} times</div>
-          </v-flex>
-          <v-flex md1 sm1 xs12 align-self-center>
-            <v-btn fab small left color="success">
-              <v-icon>add</v-icon>
-            </v-btn>
-          </v-flex>
-        </v-layout>
-        <v-divider></v-divider>
-      </v-card>
+      <v-expansion-panel popout>
+        <v-expansion-panel-content
+          v-for="goal in goals"
+          :key="goal.title"
+          :style="`border-radius: 5px 0 0 5px; border-left: 10px solid ${getColorByDifficulty(goal.difficulty)}`"
+        >
+          <template v-slot:header>
+            <v-layout row wrap class="pa-3">
+              <v-flex md3 sm3 xs12>
+                <div class="caption grey--text">Title</div>
+                <div class="mt-2">{{ goal.title }}</div>
+              </v-flex>
+              <v-flex md2 sm2 xs6>
+                <div class="caption grey--text">Difficulty</div>
+                <div class="mt-2">{{ goal.difficulty }}</div>
+              </v-flex>
+              <v-flex md2 sm2 xs6>
+                <div class="caption grey--text">Steps</div>
+                <div class="mt-2">{{ goal.steps }}</div>
+              </v-flex>
+              <v-flex md2 sm2 xs6>
+                <div class="caption grey--text">Step Duration</div>
+                <div class="mt-2">{{ goal.stepMinDuration }}m</div>
+              </v-flex>
+              <v-flex md2 sm2 xs6>
+                <div class="caption grey--text">Achived by users</div>
+                <div class="mt-2">{{ goal.added }} times</div>
+              </v-flex>
+              <v-flex md1 sm1 xs12 align-self-center>
+                <v-btn @click.self.prevent fab small left color="success">
+                  <v-icon>add</v-icon>
+                </v-btn>
+              </v-flex>
+            </v-layout>
+          </template>
+          <v-card>
+            <v-card-text>
+              <ol>
+                <li>Step 1</li>
+                <li>Step 2</li>
+                <li>Step 3</li>
+                <li>Step 4</li>
+              </ol>
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
     </v-container>
   </div>
 </template>
